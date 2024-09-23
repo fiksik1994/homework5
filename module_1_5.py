5a92877a81ee86e97c67810b27d0431313a5a844
fiksik1994 <sema.vol98@gmail.com>
2024-09-23 19:10:58 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 'ночь', 'день', 3, 9, True
+print(immutable_var)
+#immutable_var[2]=5
+#print(immutable_var) #кортеж не поддерживает обращение по элементам
+mutable_list = [0, 5, 3], 65, 85
+print(mutable_list)
+mutable_list[0][2]=350
+print(mutable_list)
